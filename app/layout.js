04a98c971f96ee6366f54ce1e590ac30d787366b
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-11-12 20:33:22 +0000

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -3,14 +3,15 @@ import "./globals.css";
 export const metadata = {
   title: "ExpenseEase",
   description: "Simple Receipt Uploads, Smarter Expense Tracking.",
+  icons: {
+    icon: "/favicon-32x32.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
