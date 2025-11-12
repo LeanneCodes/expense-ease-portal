8fa9dc43556f1a4e61d46c97b68092d4945555c6
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-11-12 23:54:54 +0000

    remove export function

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,7 @@ export const metadata = {
   title: "ExpenseEase",
   description: "Simple Receipt Uploads, Smarter Expense Tracking.",
   icons: {
-    icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", sizes: "16x16", type: "image/x-icon" },
-    ],
+    icon: "/favicon-32x32.png",
   },
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static HTML export
+  /* config options here */
 };
 
 export default nextConfig;
